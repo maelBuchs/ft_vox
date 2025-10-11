eb375e8a8d3a35968e74b26b64b163aa08bcb003
maelBuchs <mael@buchs.fr>
2025-10-11 17:02:25 +0200

    Added Pipeline.cpp/hpp

---
+++ src/Pipeline.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class Pipeline {
+public:
+  Pipeline(const std::string &vertFilepath, const std::string &fragFilepath);
+
+private:
+  static std::vector<char> readFile(const std::string &filepath);
+  void createGraphicsPipeline(const std::string &vertFilepath,
+                              const std::string &fragFilepath);
+};
